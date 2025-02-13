1fa53f95ac8cb1d04eb3febefa18416f4d741df9
Janek Lauer <git@lauer.email>
2025-02-13 18:31:03 +0100

    add modifier

--- forms.go
+++ forms.go
@@ -1,10 +1,14 @@
 package forms
 
 import (
+	"context"
 	"errors"
 	"log/slog"
 	"net/http"
+	"reflect"
 
+	"github.com/go-playground/mold/v4"
+	"github.com/go-playground/mold/v4/modifiers"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/schema"
 	"github.com/microcosm-cc/bluemonday"
@@ -14,17 +18,20 @@ type FormProcessor struct {
 	decoder  *schema.Decoder
 	validate *validator.Validate
 	policy   *bluemonday.Policy
+	modifier *mold.Transformer
 }
 
 func NewFormProcessor() (*FormProcessor, error) {
 	decoder := schema.NewDecoder()
 	validate := validator.New(validator.WithRequiredStructEnabled())
 	policy := bluemonday.StrictPolicy()
+	modifier := modifiers.New()
 
 	return NewFormProcessorInitialized(
 		decoder,
 		validate,
 		policy,
+		modifier,
 	)
 }
 
@@ -32,21 +39,32 @@ func NewFormProcessorInitialized(
 	decoder *schema.Decoder,
 	validate *validator.Validate,
 	policy *bluemonday.Policy,
+	modifier *mold.Transformer,
 ) (*FormProcessor, error) {
 	if decoder == nil || validate == nil || policy == nil {
 		return nil, errors.New("FormProcessor dependencies not fulfilled")
 	}
 
+	modifier.Register("sanitize", func(ctx context.Context, fl mold.FieldLevel) error {
+		switch fl.Field().Kind() {
+		case reflect.String:
+			fl.Field().SetString(policy.Sanitize(fl.Field().String()))
+		}
+		return nil
+	})
+
 	return &FormProcessor{
 		decoder:  decoder,
 		validate: validate,
 		policy:   policy,
+		modifier: modifier,
 	}, nil
 }
 
 var (
 	ErrorParsing    = errors.New("Error parsing form")
 	ErrorDecoding   = errors.New("Error decoding form")
+	ErrorModifying  = errors.New("Error modifying form")
 	ErrorValidating = errors.New("Error validating form")
 )
 
@@ -59,6 +77,10 @@ func (fp *FormProcessor) ProcessForm(dst interface{}, req *http.Request) error {
 	if err := fp.decoder.Decode(dst, req.PostForm); err != nil {
 		slog.Error("ProcessForm", "step", "decode", "err", err)
 		return ErrorDecoding
+	}
+	if err := fp.modifier.Struct(context.Background(), dst); err != nil {
+		slog.Error("ProcessForm", "step", "modify", "err", err)
+		return ErrorModifying
 	}
 	if err := fp.validate.Struct(dst); err != nil {
 		slog.Error("ProcessForm", "step", "validate", "err", err)
--- go.mod
+++ go.mod
@@ -12,9 +12,14 @@ require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/mold/v4 v4.5.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
+	github.com/gosimple/slug v1.15.0 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/segmentio/go-camelcase v0.0.0-20160726192923-7085f1e3c734 // indirect
+	github.com/segmentio/go-snakecase v1.2.0 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
